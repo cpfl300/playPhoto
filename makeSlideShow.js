018b1d548c9bd8ac72e09ab0968a9d01c7cbef01
cpfl300 <cpfl300@naver.com>
2014-09-10 20:04:03 +0900

    #9 사진 노출 시간을 초 단위로 입력받음

--- makeSlideShow.js
+++ makeSlideShow.js
@@ -16,7 +16,13 @@ var makeSlideShow = {
 			alert("선택된 사진이 없습니다.");
 			return;	
 		} 
-		this.makeFadeInOut();
+		var sec = 8/parseFloat(document.querySelector('#barBtn input').value); //한장 당 총 8번 보여줌
+
+		if(isNaN(sec) || !(parseFloat(document.querySelector('#barBtn input').value).toString() == document.querySelector('#barBtn input').value)){
+			alert("한 장의 사진을 \"몇초간\" 보여줄지... \n올바른 숫자를 입력해주세요");
+			return;	
+		}
+		this.makeFadeInOut(sec);
 	},
 	findChecked : function(divs){
 		divs.map(function(div){
@@ -34,14 +40,14 @@ var makeSlideShow = {
 		tempTag.download = "myVideo.webm";
 		tempTag.click();
 	},
-	makeFadeInOut : function(){
-		var vid = new Whammy.Video(10); // 비디오 프레임 rate를 인자로 넣어야 함
+	makeFadeInOut : function(sec){
+		var vid = new Whammy.Video(sec); // 비디오 프레임 rate를 인자로 넣어야 함
 		var tempCanvas = document.createElement('canvas');
 		tempCanvas.width = resource.shareElement.canvas.width;
 		tempCanvas.height = resource.shareElement.canvas.height;
 		var ctx = tempCanvas.getContext('2d');
 		
-		for(var i = 0; i < this.checkedPicture.length; i++){
+		for(var i = this.checkedPicture.length -1; i >= 0 ; i--){
 			var j = 0.2;
 			for(; j <= 1.0; j+=0.2){
 				ctx.globalAlpha = j;
